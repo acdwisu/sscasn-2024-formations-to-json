e827ba27eff806b20d148ed4e348a743b4b55395
acdwisu <achmaddwisuriyanto@gmail.com>
2024-08-27 22:44:42 +0700

    refactor: execution mode declaration to param file, include execution mode in conversion param

--- lib/src/converter/param.dart
+++ lib/src/converter/param.dart
@@ -1,5 +1,8 @@
 class FormationsToJsonParam {
   final String kodePendidikan;
+  final ExecutionMode executionMode;
 
-  FormationsToJsonParam({required this.kodePendidikan});
+  FormationsToJsonParam({required this.kodePendidikan, this.executionMode=ExecutionMode.sync});
 }
+
+enum ExecutionMode { sync, async }
