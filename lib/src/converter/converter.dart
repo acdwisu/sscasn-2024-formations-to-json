dd39e9faaab1bb89a10b7b5d691bbffdb3ff7318
acdwisu <achmaddwisuriyanto@gmail.com>
2024-08-27 22:45:18 +0700

    chore: file rename

--- lib/src/converter/converter.dart
+++ lib/src/converter/converter.dart
@@ -1,25 +1,2 @@
-import 'package:sscasn_2024_formations_to_json/src/rest_executor.dart';
-
-import 'param.dart';
-
-typedef JsonResult = List<Map>;
-
-abstract class IFormationsToJson {
-  final FormationsToJsonParam param;
-  final IRestExecutor restExecutor;
-
-  IFormationsToJson({required this.param, required this.restExecutor});
-
-  Future<JsonResult> call();
-}
-
-class FormationsToJson extends IFormationsToJson {
-  FormationsToJson({
-    required super.param,
-  }) : super(restExecutor: RestExecutor(param: param));
-
-  @override
-  Future<JsonResult> call() {
-    return restExecutor();
-  }
-}
+export 'formation_to_json.dart';
+export 'param.dart';
