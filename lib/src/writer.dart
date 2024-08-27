9efc0410abeb6609b9bfec6256238239809d86f7
acdwisu <achmaddwisuriyanto@gmail.com>
2024-08-27 13:17:20 +0700

    feat: implement writer (dat file writer)

--- lib/src/writer.dart
+++ lib/src/writer.dart
@@ -1,9 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:sscasn_2024_formations_to_json/src/converter/converter.dart';
 
 abstract class Writer {
   final JsonResult result;
+  final String writeDestination;
 
-  Writer({required this.result});
+  Writer({required this.result, required this.writeDestination});
 
   Future<void> call();
+}
+
+class DatFileWriter extends Writer {
+  DatFileWriter({required super.result, required super.writeDestination});
+
+  @override
+  Future<void> call() {
+    return File(writeDestination).writeAsString(jsonEncode(result));
+  }
 }
