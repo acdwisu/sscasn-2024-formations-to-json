6c6a3eccb80e5793093d6cbe81fdfb2f7d575372
acdwisu <achmaddwisuriyanto@gmail.com>
2024-08-27 22:43:03 +0700

    refactor: dat file writer to json file writer

--- lib/src/writer.dart
+++ lib/src/writer.dart
@@ -2,21 +2,38 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:sscasn_2024_formations_to_json/src/converter/converter.dart';
+import 'package:sscasn_2024_formations_to_json/src/converter/formation_to_json.dart';
 
 abstract class Writer {
   final JsonResult result;
-  final String writeDestination;
+  final FormationsToJsonParam param;
+  final String writeDestinationDir;
 
-  Writer({required this.result, required this.writeDestination});
+  Writer({required this.result, required this.param, required this.writeDestinationDir});
 
   Future<void> call();
 }
 
-class DatFileWriter extends Writer {
-  DatFileWriter({required super.result, required super.writeDestination});
+class JsonFileWriter extends Writer {
+  JsonFileWriter({required super.result, required super.param, required super.writeDestinationDir});
 
   @override
   Future<void> call() {
-    return File(writeDestination).writeAsString(jsonEncode(result));
+    final dir = File(writeDestinationDir);
+
+    return File(_getCorrectFileName("$writeDestinationDir/${param.kodePendidikan}", 'json'))
+        .writeAsString(jsonEncode(result));
+  }
+
+  String _getCorrectFileName(String fileName, String fileExtension) {
+    int numbering = 1;
+
+    var finalFilaName = '$fileName.$fileExtension';
+
+    while(File(finalFilaName).existsSync()) {
+      finalFilaName = "$fileName (${numbering++}).$fileExtension";
+    }
+
+    return finalFilaName;
   }
 }
